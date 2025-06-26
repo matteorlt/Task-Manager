138fb42f3cbd2676e3dc9d95a8cc12898fff9ea3
Mattéo Rannou <matteo.rannouletexier2025@campus-eni.fr>
2025-06-26 14:01:49 +0200

    .

--- server/src/models/Invitation.ts
+++ server/src/models/Invitation.ts
@@ -1,38 +1,13 @@
-import { Schema, model, Document } from 'mongoose';
+import { RowDataPacket } from 'mysql2';
 
-export interface IInvitation extends Document {
-  senderId: Schema.Types.ObjectId;
-  senderEmail: string;
-  recipientEmail: string;
+export interface Invitation extends RowDataPacket {
+  id: number;
+  sender_id: number;
+  sender_email: string;
+  sender_name: string;
+  recipient_email: string;
+  event_id: number;
   status: 'pending' | 'accepted' | 'rejected';
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-const invitationSchema = new Schema<IInvitation>(
-  {
-    senderId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    senderEmail: {
-      type: String,
-      required: true,
-    },
-    recipientEmail: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ['pending', 'accepted', 'rejected'],
-      default: 'pending',
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export default model<IInvitation>('Invitation', invitationSchema); 
+  created_at: Date;
+  updated_at: Date;
+} 
